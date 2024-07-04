42202dfc258470a90621049e36a8e35f79cd0c5b
okhorsunenko <okhorsunenko@gmail.com>
2024-07-04 22:32:31 +0200

    Task_8 Add functionality which will take name of user from gravatar service, if not will show <No name>. Decided to use js functionality to be sure that name of the user will be updated and correct all the time. It showed in the lists and on the top right(profile info).

---
+++ Todo/wwwroot/js/detailPageTodoList.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var toggleButton = document.getElementById("toggleDoneItems");
+    var isHidden = false;
+
+    toggleButton.addEventListener("click", function () {
+        var items = document.querySelectorAll("li[data-item-is-done='True']");
+        items.forEach(function (item) {
+            if (isHidden) {
+                item.style.display = "";
+            } else {
+                item.style.display = "none";
+            }
+        });
+
+        isHidden = !isHidden;
+        toggleButton.textContent = isHidden ? "Show Done Items" : "Hide Done Items";
+    });
+});
---
+++ Todo/wwwroot/js/gravatarService.js
@@ -0,0 +1,43 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    function fetchWithTimeout(url, options = {}, timeout = 5000) {
+        return new Promise((resolve, reject) => {
+            const timer = setTimeout(() => {
+                reject(new Error('Request timed out'));
+            }, timeout);
+
+            fetch(url, options)
+                .then(response => {
+                    clearTimeout(timer);
+                    resolve(response);
+                })
+                .catch(error => {
+                    clearTimeout(timer);
+                    reject(error);
+                });
+        });
+    }
+    function fetchGravatarProfile(email, element) {
+        const url = `https://gravatar.com/${email}.json`;
+
+        fetchWithTimeout(url)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                    console.log("Issue with Gravatar service");
+                }
+                return response.json();
+            })
+            .then(data => {
+                const displayName = data.entry[0].displayName || '<No name>';
+                element.textContent = displayName;
+            })
+            .catch(() => {
+                element.textContent = '<No name>';
+            });
+    }
+
+    document.querySelectorAll('.display-name').forEach(function (element) {
+        const email = element.getAttribute('data-email');
+        fetchGravatarProfile(email, element);
+    });
+});
